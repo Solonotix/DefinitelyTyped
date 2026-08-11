439ae24fb2458a37cb9c7f8b1078d32c2ebfdb64
Solonotix <tillisadam@gmail.com>
2026-08-11 08:28:31 -0600

    feat: extend _internal types for selenium-webdriver

--- types/selenium-webdriver/_internal.d.ts
+++ types/selenium-webdriver/_internal.d.ts
@@ -3,8 +3,16 @@ export type EntriesOf<T> = Array<{ [K in keyof T]: EntryOf<T, K> }[keyof T]>;
 export type MapOf<T> = Map<keyof T & string, { [K in keyof T & string]: T[K] }[keyof T & string]>;
 export type ObjectLike<T> = { [K in keyof T]?: T[K] | undefined };
 
+/** Preserves literal suggestions while allowing arbitrary numeric values. */
+export type SuggestedNumber<T extends number> = T | (number & {});
+
+/** Preserves literal suggestions while allowing arbitrary string values. */
+export type SuggestedString<T extends string> = T | (string & {});
+
+// TODO: The staged declaration defaults the argument tuple to `[]`, while the
+// existing declaration accepts an arbitrary readonly argument list by default.
 export type TypedFunction<T = unknown, A extends ReadonlyArray<unknown> = ReadonlyArray<unknown>> = (...args: A) => T;
 
 export namespace Internal {
-    export { EntryOf, EntriesOf, MapOf, ObjectLike, TypedFunction };
+    export { EntriesOf, EntryOf, MapOf, ObjectLike, SuggestedNumber, SuggestedString, TypedFunction };
 }
